c4cd0f229efe3818d60f6a5d284daa519c35dc69
n3mchu4 <thanhnam01382006@gmail.com>
2026-08-17 13:11:13 +0700

    Add logging for DagBag filling and deprecation warnings in crypto_extract_dag.py

--- dags/crypto_extract_dag.py
+++ dags/crypto_extract_dag.py
@@ -1,19 +1,19 @@
-from datetime import datetime
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 from extract.run_extract import main
 
+from datetime import datetime
+
 
 with DAG(
-    dag_id="crypto_extract_dag",
-    start_date=datetime(2026, 8, 14),
+    dag_id="crypto_extract",
+    start_date=datetime(2026, 8, 1),
     schedule="@daily",
     catchup=False,
 ) as dag:
 
-    extract_crypto = PythonOperator(
-        task_id="extract_crypto",
+    extract_task = PythonOperator(
+        task_id="extract_crypto_data",
         python_callable=main,
     )
