1870a88da8b192177bfe975104118f87233a6744
n3mchu4 <thanhnam01382006@gmail.com>
2026-08-29 13:23:07 +0700

    add schema and staging model for raw cryptocurrency prices

---
+++ dbt_project/crypto_dbt/models/staging/stg_crypto_prices.sql
@@ -0,0 +1,28 @@
+SELECT
+    id,
+    symbol,
+    name,
+    current_price,
+    market_cap,
+    market_cap_rank,
+    fully_diluted_valuation,
+    total_volume,
+    high_24h,
+    low_24h,
+    price_change_24h,
+    price_change_percentage_24h,
+    market_cap_change_24h,
+    market_cap_change_percentage_24h,
+    circulating_supply,
+    total_supply,
+    max_supply,
+    ath,
+    ath_change_percentage,
+    ath_date,
+    atl,
+    atl_change_percentage,
+    atl_date,
+    roi,
+    last_updated
+
+FROM {{ source('raw', 'raw_crypto_prices') }}
